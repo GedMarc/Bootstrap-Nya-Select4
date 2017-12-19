e6c1044eb4e1533ac15ec0f29cd81b2c96a76b6c
Marc Magon <ged_marc@hotmail.com>
2017-12-19 16:24:15 +0200

    Updates

---
+++ src/za/co/mmagon/jwebswing/angularnyabootstrapselector/NyaSelect.java
@@ -0,0 +1,319 @@
+/*
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package za.co.mmagon.jwebswing.angularnyabootstrapselector;
+
+import za.co.mmagon.jwebswing.base.angular.AngularAttributes;
+import za.co.mmagon.jwebswing.base.angular.AngularPageConfigurator;
+import za.co.mmagon.jwebswing.base.html.List;
+import za.co.mmagon.jwebswing.plugins.ComponentInformation;
+import za.co.mmagon.jwebswing.plugins.bootstrap.BootstrapPageConfigurator;
+import za.co.mmagon.jwebswing.plugins.bootstrap.forms.BSFormChildren;
+import za.co.mmagon.jwebswing.plugins.bootstrap.forms.groups.BSFormGroupChildren;
+import za.co.mmagon.jwebswing.plugins.bootstrap.forms.groups.sets.BSFormSetChildren;
+import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
+
+/**
+ * An implementation of
+ * <p>
+ *
+ * @author Marc Magon
+ * @version 1.0
+ * @since 17 Jan 2017
+ */
+@ComponentInformation(name = "Bootstrap Nya Select", description = "An Bootstrap dropdown component and replacement of vanilla select element, designed for AngularJS ",
+		url = "http://nya.io/nya-bootstrap-select/#!/", wikiUrl = "https://github.com/GedMarc/JWebSwing-BSNyaSelect/wiki")
+public class NyaSelect extends List<NyaSelectChildren, NyaSelectAttributes, NyaSelectEvents, NyaSelect> implements BSFormChildren, BSFormGroupChildren, BSFormSetChildren
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String SelectedTextAttributeText = "selected-text-format";
+
+	/**
+	 * Constructs a new instance
+	 * <p>
+	 */
+	public NyaSelect()
+	{
+		this("data.nya");
+	}
+
+	/**
+	 * Constructs a new instance
+	 *
+	 * @param bindingVariableName
+	 */
+	public NyaSelect(String bindingVariableName)
+	{
+		super(true);
+		addClass("nya-bs-select");
+		addAttribute(AngularAttributes.ngModel, bindingVariableName);
+		JQueryPageConfigurator.setRequired(this, true);
+		BootstrapPageConfigurator.setRequired(this, true);
+		AngularPageConfigurator.setRequired(this, true);
+	}
+
+	/**
+	 * Sets this select option as multiple Binds as an array, make the dto object a List
+	 *
+	 * @param multiple
+	 *
+	 * @return
+	 */
+	public NyaSelect setMultiple(boolean multiple)
+	{
+		if (multiple)
+		{
+			addAttribute("multiple", null);
+		}
+		else
+		{
+			getAttributes().remove("multiple");
+		}
+		return this;
+	}
+
+	/**
+	 * Provide a input box to search through all option content only if live-search="true"
+	 *
+	 * @param liveSearch
+	 *
+	 * @return
+	 */
+	public NyaSelect setLiveSearch(boolean liveSearch)
+	{
+		if (liveSearch)
+		{
+			addAttribute("live-search", Boolean.TRUE.toString());
+		}
+		else
+		{
+			getAttributes().remove("live-search");
+		}
+		return this;
+	}
+
+	/**
+	 * The control is considered valid if ngModel value is defined or not an empty array(for multiple)
+	 *
+	 * @param required
+	 *
+	 * @return
+	 */
+	public NyaSelect setRequired(boolean required)
+	{
+		if (required)
+		{
+			addAttribute("required", Boolean.TRUE.toString());
+		}
+		else
+		{
+			getAttributes().remove("required");
+		}
+		return this;
+	}
+
+	/**
+	 * if the expression is truthy, then the whole control will be disabled.
+	 *
+	 * @param expression
+	 *
+	 * @return
+	 */
+	public NyaSelect setDisabled(String expression)
+	{
+		if (expression != null && !expression.isEmpty())
+		{
+			addAttribute("disabled", expression);
+		}
+		else
+		{
+			getAttributes().remove("disabled");
+		}
+		return this;
+	}
+
+	/**
+	 * Sets max number which the dropdown-menu can show. if the number of options exceed the limit. an scrollbar will be shown.
+	 *
+	 * @param displaysize
+	 *
+	 * @return
+	 */
+	public NyaSelect setDisplaySize(Integer displaysize)
+	{
+		if (displaysize != null && displaysize != 0)
+		{
+			addAttribute("size", displaysize.toString());
+		}
+		else
+		{
+			getAttributes().remove("size");
+		}
+		return this;
+	}
+
+	/**
+	 * provide a replacement for default text when nothing is selected.
+	 *
+	 * @param title
+	 *
+	 * @return
+	 */
+	public NyaSelect setTitle(String title)
+	{
+		if (title != null && !title.isEmpty())
+		{
+			addAttribute("title", title);
+		}
+		else
+		{
+			getAttributes().remove("title");
+		}
+		return this;
+	}
+
+	/**
+	 * Sets this select option as multiple Binds as an array, make the dto object a List
+	 *
+	 * @param actionsBox
+	 *
+	 * @return
+	 */
+	public NyaSelect setActionsBox(boolean actionsBox)
+	{
+		if (actionsBox)
+		{
+			addAttribute("actions-box", "true");
+		}
+		else
+		{
+			getAttributes().remove("actions-box");
+		}
+		return this;
+	}
+
+	/**
+	 * if sets to value, the dropdown-toggle button will show content user has selected, this is default value.
+	 *
+	 * @param selectedTextAsValue
+	 *
+	 * @return
+	 */
+	public NyaSelect setSelectedTextValue(boolean selectedTextAsValue)
+	{
+		if (selectedTextAsValue)
+		{
+			addAttribute(SelectedTextAttributeText, "value");
+		}
+		else
+		{
+			getAttributes().remove(SelectedTextAttributeText);
+		}
+		return this;
+	}
+
+	/**
+	 * if sets to count, the dropdown-toggle button will show the number of options user has selected.
+	 *
+	 * @param selectedTextAsCount
+	 *
+	 * @return
+	 */
+	public NyaSelect setSelectedTextCount(boolean selectedTextAsCount)
+	{
+		if (selectedTextAsCount)
+		{
+			addAttribute(SelectedTextAttributeText, "count");
+		}
+		else
+		{
+			getAttributes().remove(SelectedTextAttributeText);
+		}
+		return this;
+	}
+
+	/**
+	 * Show Menu Arrow
+	 * <p>
+	 * To enable an menu arrow on dropdown menu. just add a class show-menu-arrow on nya-bs-select element.
+	 *
+	 * @param showMenuIcon
+	 *
+	 * @return
+	 */
+	public NyaSelect setShowMenuIcon(boolean showMenuIcon)
+	{
+		if (showMenuIcon)
+		{
+			addClass("show-menu-arrow");
+		}
+		else
+		{
+			removeClass("show-menu-arrow");
+		}
+		return this;
+	}
+
+	/**
+	 * if sets to count greater than x, the dropdown-toggle button will show the number of user selected options when the number of options greater than x. otherwise, show the content.
+	 *
+	 * @param selectedTextAsCount
+	 * @param countThan
+	 *
+	 * @return
+	 */
+	public NyaSelect setSelectedTextCountGreaterThan(boolean selectedTextAsCount, int countThan)
+	{
+		if (selectedTextAsCount)
+		{
+			addAttribute(SelectedTextAttributeText, "count>" + countThan);
+		}
+		else
+		{
+			getAttributes().remove(SelectedTextAttributeText);
+		}
+		return this;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if (getClass() != obj.getClass())
+		{
+			return false;
+		}
+		return super.equals(obj);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int hash = 7;
+		hash = 79 * hash + (this.getID().hashCode());
+		return hash;
+	}
+
+}
---
+++ src/za/co/mmagon/jwebswing/angularnyabootstrapselector/NyaSelectItem.java
@@ -0,0 +1,214 @@
+/* 
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package za.co.mmagon.jwebswing.angularnyabootstrapselector;
+
+import za.co.mmagon.jwebswing.base.html.*;
+import za.co.mmagon.jwebswing.plugins.bootstrap.dropdown.BSComponentDropDownOptions;
+
+/**
+ * @author GedMarc
+ * @since 04 Feb 2017
+ */
+public class NyaSelectItem extends ListItem implements NyaSelectChildren
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private String label;
+	private String text;
+	private String iconClass;
+	private String value;
+	private String checkClass;
+	private String subText;
+
+	public NyaSelectItem()
+	{
+	}
+
+	public NyaSelectItem(String text, String iconClass, String value, String checkClass, String subText)
+	{
+		this.text = text;
+		this.iconClass = iconClass;
+		this.value = value;
+		this.checkClass = checkClass;
+		this.subText = subText;
+	}
+
+	public String getLabel()
+	{
+		return label;
+	}
+
+	public NyaSelectItem setLabel(String label)
+	{
+		this.label = label;
+		return this;
+	}
+
+	public String getText()
+	{
+		return text;
+	}
+
+	@Override
+	public NyaSelectItem setText(String text)
+	{
+		this.text = text;
+		return this;
+	}
+
+	public String getIconClass()
+	{
+		return iconClass;
+	}
+
+	public NyaSelectItem setIconClass(String iconClass)
+	{
+		this.iconClass = iconClass;
+		return this;
+	}
+
+	public String getValue()
+	{
+		return value;
+	}
+
+	public NyaSelectItem setValue(String value)
+	{
+		this.value = value;
+		return this;
+	}
+
+	public String getCheckClass()
+	{
+		return checkClass;
+	}
+
+	public NyaSelectItem setCheckClass(String checkClass)
+	{
+		this.checkClass = checkClass;
+		return this;
+	}
+
+	public String getSubText()
+	{
+		return subText;
+	}
+
+	public void setSubText(String subText)
+	{
+		this.subText = subText;
+	}
+
+	@Override
+	public void preConfigure()
+	{
+		if (!isConfigured())
+		{
+			addClass("nya-bs-option");
+			addClass(BSComponentDropDownOptions.Dropdown_Item);
+
+			Link link = new Link();
+			add(link);
+
+			Span labelSpan = new Span();
+
+			if (getIconClass() != null && !getIconClass().isEmpty())
+			{
+				Span iconSpan = new Span();
+				iconSpan.addClass(getIconClass());
+				labelSpan.add(iconSpan);
+			}
+			labelSpan.add(new Paragraph(getText()).setTextOnly(true));
+
+			link.add(labelSpan);
+
+			if (getSubText() != null && !getSubText().isEmpty())
+			{
+				labelSpan.add(new SmallText(getSubText()));
+			}
+
+			if (getValue() != null && !getValue().isEmpty())
+			{
+				addAttribute("data-value", getValue());
+			}
+
+			if (getCheckClass() != null && !getCheckClass().isEmpty())
+			{
+				Span iconSpan = new Span();
+				iconSpan.addClass(getCheckClass());
+				iconSpan.addClass("check-mark");
+				link.add(iconSpan);
+			}
+		}
+		super.preConfigure();
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o)
+		{
+			return true;
+		}
+		if (!(o instanceof NyaSelectItem))
+		{
+			return false;
+		}
+		if (!super.equals(o))
+		{
+			return false;
+		}
+
+		NyaSelectItem that = (NyaSelectItem) o;
+
+		if (getLabel() != null ? !getLabel().equals(that.getLabel()) : that.getLabel() != null)
+		{
+			return false;
+		}
+		if (getText() != null ? !getText().equals(that.getText()) : that.getText() != null)
+		{
+			return false;
+		}
+		if (getIconClass() != null ? !getIconClass().equals(that.getIconClass()) : that.getIconClass() != null)
+		{
+			return false;
+		}
+		if (getValue() != null ? !getValue().equals(that.getValue()) : that.getValue() != null)
+		{
+			return false;
+		}
+		if (getCheckClass() != null ? !getCheckClass().equals(that.getCheckClass()) : that.getCheckClass() != null)
+		{
+			return false;
+		}
+		return getSubText() != null ? getSubText().equals(that.getSubText()) : that.getSubText() == null;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		int result = super.hashCode();
+		result = 31 * result + (getLabel() != null ? getLabel().hashCode() : 0);
+		result = 31 * result + (getText() != null ? getText().hashCode() : 0);
+		result = 31 * result + (getIconClass() != null ? getIconClass().hashCode() : 0);
+		result = 31 * result + (getValue() != null ? getValue().hashCode() : 0);
+		result = 31 * result + (getCheckClass() != null ? getCheckClass().hashCode() : 0);
+		result = 31 * result + (getSubText() != null ? getSubText().hashCode() : 0);
+		return result;
+	}
+}

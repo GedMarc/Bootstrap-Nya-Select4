a9447ba5bd89f360421315acee9624a4de7ac145
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:29:23 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/jre11/java/module-info.java
@@ -0,0 +1,27 @@
+import com.jwebmp.core.base.angular.services.IAngularModule;
+import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+import com.jwebmp.plugins.angularnyabootstrapselector.NyaSelectAngularModule;
+import com.jwebmp.plugins.angularnyabootstrapselector.NysSelectBootstrapConfigurator;
+import com.jwebmp.plugins.angularnyabootstrapselector.implementations.BSNyaSelect4ExclusionsModule;
+
+module com.jwebmp.plugins.angularnyabootstrap4selector {
+	exports com.jwebmp.plugins.angularnyabootstrapselector;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.logmaster;
+	requires com.fasterxml.jackson.annotation;
+
+	requires java.validation;
+	requires com.jwebmp.plugins.bootstrap4;
+	requires com.jwebmp.guicedinjection;
+
+	provides IPageConfigurator with NysSelectBootstrapConfigurator;
+	provides IAngularModule with NyaSelectAngularModule;
+
+	provides IGuiceScanModuleExclusions with BSNyaSelect4ExclusionsModule;
+	provides IGuiceScanJarExclusions with BSNyaSelect4ExclusionsModule;
+
+	opens com.jwebmp.plugins.angularnyabootstrapselector to com.fasterxml.jackson.databind, com.jwebmp.core;
+}

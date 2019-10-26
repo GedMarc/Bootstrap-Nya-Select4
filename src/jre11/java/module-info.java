29ef9d9e11c9cc963c437991259267907f0942a7
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:34:50 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -2,19 +2,19 @@ module com.jwebmp.plugins.bs4.nyaselect {
 	exports com.jwebmp.plugins.bs4.nyaselect;
 
 	requires com.jwebmp.core;
-	requires com.jwebmp.logmaster;
+	requires com.guicedee.logmaster;
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
 	requires com.jwebmp.plugins.bootstrap4;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 	requires com.jwebmp.core.angularjs;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.bs4.nyaselect.NyaSelectPageConfigurator;
 	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.bs4.nyaselect.NyaSelectAngularModule;
 
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.bs4.nyaselect.implementations.BS4NyaSelectExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.bs4.nyaselect.implementations.BS4NyaSelectExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.bs4.nyaselect.implementations.BS4NyaSelectExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.bs4.nyaselect.implementations.BS4NyaSelectExclusionsModule;
 
 	opens com.jwebmp.plugins.bs4.nyaselect to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
